a809370e6df14dbb0482cdade3c0aa712ef744d2
Bharathrao <>
2019-08-21 11:04:37 +0530

    Added the Sticky type for Navbar

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,15 @@ import NavBar from "./NavBar"
 class About extends Component {
   render() {
     return (
-        <div>
-          <h2>About</h2>
-        </div>
+      <div>
+        <h2>About</h2>
+        <p>To switch directories, type <kbd>cd</kbd> followed by the name of the directory.</p>ß
+        <p>To edit settings, press <kbd><kbd>ctrl</kbd> + <kbd>,</kbd></kbd></p>
+        <code>&lt;section&gt;
+          #include iostream.h
+          #include conio.h
+        </code>
+      </div>
     );
   }
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,7 +20,7 @@ class NavBar extends Component {
     }
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg  navbar-dark bg-dark sticky-top">
                 <a className="navbar-brand" href="#">Tasks</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
